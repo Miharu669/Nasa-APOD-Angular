036f58e8a2b52762e3db9e9b5e84ff2d358a83eb
Vero Doel <veronicadoelfuentes@gmail.com>
2025-02-03 20:17:37 +0100

    minor fix, retired deprecated stuff

--- src/app/components/nasa-apod/nasa-apod.component.ts
+++ src/app/components/nasa-apod/nasa-apod.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NasaService } from '../../services/nasa.service';
 
 @Component({
